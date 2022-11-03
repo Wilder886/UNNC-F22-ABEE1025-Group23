ca3436aac43a03b9a53938f221db9b34ce5d06fa
Wilder886 <113965788+Wilder886@users.noreply.github.com>
2022-11-03 15:43:06 +0800

    Delete test3.py

--- test3.py
+++
@@ -1,6 +0,0 @@
-from parametric_simulation import run_one_simulation_helper
-eplus_run_path = './energyplus9.5/energyplus'
-idf_path = './1ZoneUncontrolled_win_1.idf'
-parameter_key =['WindowMaterial:SimpleGlazingSystem',
-      'SimpleWindow:DOUBLE PANE WINDOW']
-parameter_vals=0.1
